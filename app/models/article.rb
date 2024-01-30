7303bee64971dd098d51792391eacee1a855f7d5
ZacharyDuaibes <112868650+ZacharyDuaibes@users.noreply.github.com>
2024-01-30 01:44:32 +0000

    Search Functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+
+    def self.search(squery)
+        where("title LIKE ? OR content LIKE ?", "%#{squery}%", "%#{squery}%")
+    end
+
 end
