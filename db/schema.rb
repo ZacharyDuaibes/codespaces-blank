6486d570641d91d942213bc7efd0ecdb82d00a9f
ZacharyDuaibes <112868650+ZacharyDuaibes@users.noreply.github.com>
2024-01-30 03:30:21 +0000

    Updated schema and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_021743) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_022726) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.date "date"
   end
 
 end
